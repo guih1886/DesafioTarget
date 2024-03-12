6261ffcf03af2652133c63b74bad3836b202f25e
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2024-03-12 13:56:57 -0300

    project creation
    project creation

---
+++ DesafioTarget/Principal.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace DesafioTarget
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            this.Grb_2 = new System.Windows.Forms.GroupBox();
+            this.Lbl_ResultFibonacci = new System.Windows.Forms.Label();
+            this.Lbl_Numbers = new System.Windows.Forms.Label();
+            this.Btn_Fibonacci = new System.Windows.Forms.Button();
+            this.Lbl_Fibonacci = new System.Windows.Forms.Label();
+            this.Txt_Fibonacci = new System.Windows.Forms.TextBox();
+            this.Grb_1 = new System.Windows.Forms.GroupBox();
+            this.Lbl_Resposta1 = new System.Windows.Forms.Label();
+            this.Grb_3 = new System.Windows.Forms.GroupBox();
+            this.Lbl_F = new System.Windows.Forms.Label();
+            this.Lbl_E = new System.Windows.Forms.Label();
+            this.Lbl_D = new System.Windows.Forms.Label();
+            this.Lbl_C = new System.Windows.Forms.Label();
+            this.Lbl_B = new System.Windows.Forms.Label();
+            this.Lbl_A = new System.Windows.Forms.Label();
+            this.Grb_4 = new System.Windows.Forms.GroupBox();
+            this.Lbl_Resposta4 = new System.Windows.Forms.Label();
+            this.Grb_5 = new System.Windows.Forms.GroupBox();
+            this.Lbl_ResultadoPalavra = new System.Windows.Forms.Label();
+            this.Txt_Palavra = new System.Windows.Forms.TextBox();
+            this.Lbl_Palavra = new System.Windows.Forms.Label();
+            this.Btn_Palavra = new System.Windows.Forms.Button();
+            this.Grb_2.SuspendLayout();
+            this.Grb_1.SuspendLayout();
+            this.Grb_3.SuspendLayout();
+            this.Grb_4.SuspendLayout();
+            this.Grb_5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Grb_2
+            // 
+            this.Grb_2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.Grb_2.Controls.Add(this.Lbl_ResultFibonacci);
+            this.Grb_2.Controls.Add(this.Lbl_Numbers);
+            this.Grb_2.Controls.Add(this.Btn_Fibonacci);
+            this.Grb_2.Controls.Add(this.Lbl_Fibonacci);
+            this.Grb_2.Controls.Add(this.Txt_Fibonacci);
+            this.Grb_2.Location = new System.Drawing.Point(13, 97);
+            this.Grb_2.Name = "Grb_2";
+            this.Grb_2.Size = new System.Drawing.Size(324, 505);
+            this.Grb_2.TabIndex = 0;
+            this.Grb_2.TabStop = false;
+            this.Grb_2.Text = "2 - Fibonacci";
+            // 
+            // Lbl_ResultFibonacci
+            // 
+            this.Lbl_ResultFibonacci.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_ResultFibonacci.Location = new System.Drawing.Point(12, 66);
+            this.Lbl_ResultFibonacci.Name = "Lbl_ResultFibonacci";
+            this.Lbl_ResultFibonacci.Size = new System.Drawing.Size(306, 23);
+            this.Lbl_ResultFibonacci.TabIndex = 4;
+            // 
+            // Lbl_Numbers
+            // 
+            this.Lbl_Numbers.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.Lbl_Numbers.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Lbl_Numbers.Location = new System.Drawing.Point(6, 102);
+            this.Lbl_Numbers.Name = "Lbl_Numbers";
+            this.Lbl_Numbers.Size = new System.Drawing.Size(312, 389);
+            this.Lbl_Numbers.TabIndex = 3;
+            // 
+            // Btn_Fibonacci
+            // 
+            this.Btn_Fibonacci.Image = global::DesafioTarget.Properties.Resources.ok;
+            this.Btn_Fibonacci.Location = new System.Drawing.Point(230, 31);
+            this.Btn_Fibonacci.Name = "Btn_Fibonacci";
+            this.Btn_Fibonacci.Size = new System.Drawing.Size(24, 24);
+            this.Btn_Fibonacci.TabIndex = 2;
+            this.Btn_Fibonacci.UseVisualStyleBackColor = true;
+            this.Btn_Fibonacci.Click += new System.EventHandler(this.Btn_Fibonacci_Click);
+            // 
+            // Lbl_Fibonacci
+            // 
+            this.Lbl_Fibonacci.AutoSize = true;
+            this.Lbl_Fibonacci.Location = new System.Drawing.Point(7, 17);
+            this.Lbl_Fibonacci.Name = "Lbl_Fibonacci";
+            this.Lbl_Fibonacci.Size = new System.Drawing.Size(250, 13);
+            this.Lbl_Fibonacci.TabIndex = 1;
+            this.Lbl_Fibonacci.Text = "Informe o número para gerar a sequência Fibonacci\r\n";
+            // 
+            // Txt_Fibonacci
+            // 
+            this.Txt_Fibonacci.Location = new System.Drawing.Point(10, 33);
+            this.Txt_Fibonacci.Name = "Txt_Fibonacci";
+            this.Txt_Fibonacci.Size = new System.Drawing.Size(216, 20);
+            this.Txt_Fibonacci.TabIndex = 0;
+            // 
+            // Grb_1
+            // 
+            this.Grb_1.Controls.Add(this.Lbl_Resposta1);
+            this.Grb_1.Location = new System.Drawing.Point(13, 12);
+            this.Grb_1.Name = "Grb_1";
+            this.Grb_1.Size = new System.Drawing.Size(324, 77);
+            this.Grb_1.TabIndex = 1;
+            this.Grb_1.TabStop = false;
+            this.Grb_1.Text = "1 - Código";
+            // 
+            // Lbl_Resposta1
+            // 
+            this.Lbl_Resposta1.AutoSize = true;
+            this.Lbl_Resposta1.Location = new System.Drawing.Point(10, 35);
+            this.Lbl_Resposta1.Name = "Lbl_Resposta1";
+            this.Lbl_Resposta1.Size = new System.Drawing.Size(187, 13);
+            this.Lbl_Resposta1.TabIndex = 0;
+            this.Lbl_Resposta1.Text = "O valor final da váriavel soma será 91.\r\n";
+            // 
+            // Grb_3
+            // 
+            this.Grb_3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Grb_3.Controls.Add(this.Lbl_F);
+            this.Grb_3.Controls.Add(this.Lbl_E);
+            this.Grb_3.Controls.Add(this.Lbl_D);
+            this.Grb_3.Controls.Add(this.Lbl_C);
+            this.Grb_3.Controls.Add(this.Lbl_B);
+            this.Grb_3.Controls.Add(this.Lbl_A);
+            this.Grb_3.Location = new System.Drawing.Point(343, 12);
+            this.Grb_3.Name = "Grb_3";
+            this.Grb_3.Size = new System.Drawing.Size(620, 197);
+            this.Grb_3.TabIndex = 2;
+            this.Grb_3.TabStop = false;
+            this.Grb_3.Text = "3 - Lógicas";
+            // 
+            // Lbl_F
+            // 
+            this.Lbl_F.AutoSize = true;
+            this.Lbl_F.Location = new System.Drawing.Point(6, 166);
+            this.Lbl_F.Name = "Lbl_F";
+            this.Lbl_F.Size = new System.Drawing.Size(602, 26);
+            this.Lbl_F.TabIndex = 5;
+            this.Lbl_F.Text = "F - O próximo número será 20, a única lógica que consigo identificar é a de seque" +
+    "ncia, onde o número anterior é somado de 1.\r\n\r\n";
+            // 
+            // Lbl_E
+            // 
+            this.Lbl_E.AutoSize = true;
+            this.Lbl_E.Location = new System.Drawing.Point(6, 136);
+            this.Lbl_E.Name = "Lbl_E";
+            this.Lbl_E.Size = new System.Drawing.Size(277, 13);
+            this.Lbl_E.TabIndex = 4;
+            this.Lbl_E.Text = "E - O próximo número será 13, a sequência é a fibonacci.\r\n";
+            // 
+            // Lbl_D
+            // 
+            this.Lbl_D.AutoSize = true;
+            this.Lbl_D.Location = new System.Drawing.Point(6, 106);
+            this.Lbl_D.Name = "Lbl_D";
+            this.Lbl_D.Size = new System.Drawing.Size(428, 13);
+            this.Lbl_D.TabIndex = 3;
+            this.Lbl_D.Text = "D - O próximo número será 256, a sequência é composta pelo último número elevado " +
+    "a 4.\r\n";
+            // 
+            // Lbl_C
+            // 
+            this.Lbl_C.AutoSize = true;
+            this.Lbl_C.Location = new System.Drawing.Point(6, 76);
+            this.Lbl_C.Name = "Lbl_C";
+            this.Lbl_C.Size = new System.Drawing.Size(484, 26);
+            this.Lbl_C.TabIndex = 2;
+            this.Lbl_C.Text = "C - O próximo número será 49, a sequência é composta pelos números 1 ao 9 elevado" +
+    "s ao quadrado.\r\n\r\n";
+            // 
+            // Lbl_B
+            // 
+            this.Lbl_B.AutoSize = true;
+            this.Lbl_B.Location = new System.Drawing.Point(6, 46);
+            this.Lbl_B.Name = "Lbl_B";
+            this.Lbl_B.Size = new System.Drawing.Size(419, 13);
+            this.Lbl_B.TabIndex = 1;
+            this.Lbl_B.Text = "B - O próximo número será 128, a sequência é formada pelo último número elevado a" +
+    " 2.\r\n";
+            // 
+            // Lbl_A
+            // 
+            this.Lbl_A.AutoSize = true;
+            this.Lbl_A.Location = new System.Drawing.Point(6, 16);
+            this.Lbl_A.Name = "Lbl_A";
+            this.Lbl_A.Size = new System.Drawing.Size(367, 13);
+            this.Lbl_A.TabIndex = 0;
+            this.Lbl_A.Text = "A - O próximo número será 9, a sequência é composta por números ímpares.";
+            // 
+            // Grb_4
+            // 
+            this.Grb_4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Grb_4.Controls.Add(this.Lbl_Resposta4);
+            this.Grb_4.Location = new System.Drawing.Point(344, 216);
+            this.Grb_4.Name = "Grb_4";
+            this.Grb_4.Size = new System.Drawing.Size(619, 117);
+            this.Grb_4.TabIndex = 3;
+            this.Grb_4.TabStop = false;
+            this.Grb_4.Text = "4 - Lâmpadas";
+            // 
+            // Lbl_Resposta4
+            // 
+            this.Lbl_Resposta4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Lbl_Resposta4.Location = new System.Drawing.Point(7, 20);
+            this.Lbl_Resposta4.Name = "Lbl_Resposta4";
+            this.Lbl_Resposta4.Size = new System.Drawing.Size(600, 87);
+            this.Lbl_Resposta4.TabIndex = 0;
+            this.Lbl_Resposta4.Text = resources.GetString("Lbl_Resposta4.Text");
+            // 
+            // Grb_5
+            // 
+            this.Grb_5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Grb_5.Controls.Add(this.Btn_Palavra);
+            this.Grb_5.Controls.Add(this.Lbl_ResultadoPalavra);
+            this.Grb_5.Controls.Add(this.Txt_Palavra);
+            this.Grb_5.Controls.Add(this.Lbl_Palavra);
+            this.Grb_5.Location = new System.Drawing.Point(343, 339);
+            this.Grb_5.Name = "Grb_5";
+            this.Grb_5.Size = new System.Drawing.Size(620, 263);
+            this.Grb_5.TabIndex = 4;
+            this.Grb_5.TabStop = false;
+            this.Grb_5.Text = "5 - Inversor de Caracteres";
+            // 
+            // Lbl_ResultadoPalavra
+            // 
+            this.Lbl_ResultadoPalavra.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Lbl_ResultadoPalavra.Location = new System.Drawing.Point(7, 64);
+            this.Lbl_ResultadoPalavra.Name = "Lbl_ResultadoPalavra";
+            this.Lbl_ResultadoPalavra.Size = new System.Drawing.Size(607, 36);
+            this.Lbl_ResultadoPalavra.TabIndex = 2;
+            this.Lbl_ResultadoPalavra.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Txt_Palavra
+            // 
+            this.Txt_Palavra.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Txt_Palavra.Location = new System.Drawing.Point(7, 37);
+            this.Txt_Palavra.MaxLength = 100;
+            this.Txt_Palavra.Name = "Txt_Palavra";
+            this.Txt_Palavra.Size = new System.Drawing.Size(576, 20);
+            this.Txt_Palavra.TabIndex = 1;
+            // 
+            // Lbl_Palavra
+            // 
+            this.Lbl_Palavra.AutoSize = true;
+            this.Lbl_Palavra.Location = new System.Drawing.Point(7, 20);
+            this.Lbl_Palavra.Name = "Lbl_Palavra";
+            this.Lbl_Palavra.Size = new System.Drawing.Size(155, 13);
+            this.Lbl_Palavra.TabIndex = 0;
+            this.Lbl_Palavra.Text = "Insira a palavra para a inversão";
+            // 
+            // Btn_Palavra
+            // 
+            this.Btn_Palavra.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Btn_Palavra.Image = global::DesafioTarget.Properties.Resources.ok;
+            this.Btn_Palavra.Location = new System.Drawing.Point(589, 35);
+            this.Btn_Palavra.Name = "Btn_Palavra";
+            this.Btn_Palavra.Size = new System.Drawing.Size(24, 24);
+            this.Btn_Palavra.TabIndex = 3;
+            this.Btn_Palavra.UseVisualStyleBackColor = true;
+            this.Btn_Palavra.Click += new System.EventHandler(this.Btn_Palavra_Click);
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(971, 614);
+            this.Controls.Add(this.Grb_5);
+            this.Controls.Add(this.Grb_4);
+            this.Controls.Add(this.Grb_3);
+            this.Controls.Add(this.Grb_1);
+            this.Controls.Add(this.Grb_2);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Principal";
+            this.Grb_2.ResumeLayout(false);
+            this.Grb_2.PerformLayout();
+            this.Grb_1.ResumeLayout(false);
+            this.Grb_1.PerformLayout();
+            this.Grb_3.ResumeLayout(false);
+            this.Grb_3.PerformLayout();
+            this.Grb_4.ResumeLayout(false);
+            this.Grb_5.ResumeLayout(false);
+            this.Grb_5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Grb_2;
+        private System.Windows.Forms.Label Lbl_Fibonacci;
+        private System.Windows.Forms.TextBox Txt_Fibonacci;
+        private System.Windows.Forms.Button Btn_Fibonacci;
+        private System.Windows.Forms.Label Lbl_Numbers;
+        private System.Windows.Forms.Label Lbl_ResultFibonacci;
+        private System.Windows.Forms.GroupBox Grb_1;
+        private System.Windows.Forms.Label Lbl_Resposta1;
+        private System.Windows.Forms.GroupBox Grb_3;
+        private System.Windows.Forms.Label Lbl_D;
+        private System.Windows.Forms.Label Lbl_C;
+        private System.Windows.Forms.Label Lbl_B;
+        private System.Windows.Forms.Label Lbl_A;
+        private System.Windows.Forms.Label Lbl_F;
+        private System.Windows.Forms.Label Lbl_E;
+        private System.Windows.Forms.GroupBox Grb_4;
+        private System.Windows.Forms.Label Lbl_Resposta4;
+        private System.Windows.Forms.GroupBox Grb_5;
+        private System.Windows.Forms.TextBox Txt_Palavra;
+        private System.Windows.Forms.Label Lbl_Palavra;
+        private System.Windows.Forms.Label Lbl_ResultadoPalavra;
+        private System.Windows.Forms.Button Btn_Palavra;
+    }
+}
---
+++ DesafioTarget/Principal.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesafioTarget
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Fibonacci_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int number = Convert.ToInt32(Txt_Fibonacci.Text);
+                List<string> fibonacci = GenerateFibonacciSequence(number);
+                Lbl_Numbers.Text = string.Join(", ", fibonacci);
+                NumberIsInFibonacci(number, fibonacci);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Entre com um número válido.", "Desafio Target", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void NumberIsInFibonacci(int number, List<string> fibonacci)
+        {
+            foreach (string item in fibonacci)
+            {
+                if (number.ToString() == item)
+                {
+                    Lbl_ResultFibonacci.ForeColor = Color.Green;
+                    Lbl_ResultFibonacci.Text = $"O número {number} está na sequencia de Fibonacci.";
+                    return;
+                }
+            }
+            Lbl_ResultFibonacci.ForeColor = Color.Red;
+            Lbl_ResultFibonacci.Text = $"O número {number} não está na sequencia de Fibonacci.";
+        }
+
+        private List<string> GenerateFibonacciSequence(int count)
+        {
+            List<string> fibonacci = new List<string>();
+            if (count == 0)
+            {
+                MessageBox.Show("Entre com um número válido.", "Desafio Target", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                fibonacci.Add("0");
+                fibonacci.Add("1");
+                for (int i = 2; i < count; i++)
+                {
+                    if (i >= 2 && i - 1 < fibonacci.Count && i - 2 < fibonacci.Count)
+                    {
+                        BigInteger nextNumber = BigInteger.Parse(fibonacci[i - 1]) + BigInteger.Parse(fibonacci[i - 2]);
+                        string nextNumberStr = nextNumber.ToString();
+                        fibonacci.Add(nextNumberStr);
+                    }
+                }
+                return fibonacci;
+            }
+            return fibonacci;
+        }
+
+        private void Btn_Palavra_Click(object sender, EventArgs e)
+        {
+            string word = Txt_Palavra.Text;
+            string reversedWord = ReversedWord(word);
+            Lbl_ResultadoPalavra.Text = reversedWord;
+        }
+
+        private string ReversedWord(string word)
+        {
+            string reversed = "";
+            for (int i = word.Length -1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+            return reversed;
+        }
+    }
+}
